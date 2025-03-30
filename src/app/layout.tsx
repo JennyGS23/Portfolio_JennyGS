f1d75c4d97a0f8873645d6c60e35d77d513e438b
JENNIFER GONZALEZ SOLIS <gonzalezsolisjennifer@gmail.com>
2025-03-30 12:52:03 -0600

    Missing changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,13 +6,13 @@ export const metadata = {
 }
 
 export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode
-}) {
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
